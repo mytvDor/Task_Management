827ea9f6de102be302c41de2eaa823888e3605ce
mytvDor <159150094+mytvDor@users.noreply.github.com>
2024-11-06 16:44:56 +0530

    Update weatherApi.ts

--- src/services/weatherApi.ts
+++ src/services/weatherApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+const API_KEY = import.meta.env.WEATHER;
 
 export const fetchWeatherData = async (city: string) => {
   try {
